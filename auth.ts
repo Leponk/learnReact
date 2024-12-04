7658dd198e6f2376ebb9ecb8b3979b1f7e16b169
k.pon.k@Windows <keitaro.katagiri@gmail.com>
2024-12-05 05:23:20 +0900

    fix: Auth情報の追加

--- auth.ts
+++ auth.ts
@@ -17,6 +17,7 @@ async function getUser(email: string): Promise<User | undefined> {
 }
 
 export const { auth, signIn, signOut } = NextAuth({
+    secret: process.env.SECRET,
     ...authConfig,
     providers: [
         Credentials({
